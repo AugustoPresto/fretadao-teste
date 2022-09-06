a1fe2f878d5b2ec364e28bd473ffd86c73b54df2
Augusto <augustodesouzacardoso@gmail.com>
2022-09-06 10:51:00 -0300

    Rspec, factory bot config, created tests for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :name, :github_url, presence: true
+  validates :github_url, uniqueness: true
 
   def self.search_all_fields(query)
     results = where('name LIKE :search
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { FactoryBot.create(:user) }
+
+  let!(:user1) { create(:user, name: "AugustoPresto", github_url: "http://www.github.com/AugustoPresto") }
+  let!(:user2) { create(:user, name: "Augusto", github_url: "http://www.github.com/Presto") }
+  let!(:user3) { create(:user, name: "Roberto Alvarez", github_url: "http://www.github.com/robertoalvarez") }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  describe ".search_all_fields" do
+    it "Returns all results with given query" do
+      users = User.search_all_fields("Augusto")
+      expect(users.size).to eq(2) 
+      expect(users).to all(be_a(User))
+    end
+
+    it "Returns 'No matches found' when no user is found" do
+      users = User.search_all_fields("matz")
+      expect(users).to eq("No matches found")
+    end
+  end
 end
