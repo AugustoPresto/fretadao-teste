de8107e828e5a67ac77d33fa20f29a2cbd145705
Augusto <augustodesouzacardoso@gmail.com>
2022-09-06 10:59:03 -0300

    added methods to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show, :destroy]
+
   def index
     if params[:query].present?
       @users = User.search_all_fields(params[:query])
@@ -9,23 +11,39 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path 
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @user.update(user_params)
+    redirect_to root_path
   end
 
   def destroy
+    @user.destroy
+    redirect_to root_path
   end
 
   private
 
     def users_params
       params.require(:user).permit(:name, :nickname, :github_url, :organization, :location)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
     end
 end
