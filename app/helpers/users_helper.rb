c94aa33523fcea82fc7de489e72f2125e4146731
Augusto <augustodesouzacardoso@gmail.com>
2022-09-06 14:39:39 -0300

    users helper enhancements

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,16 +1,22 @@
 module UsersHelper
   def users_list
-    if @users.class == Array
-      @users.collect do |user|
-        tag.div(class: "d-flex justify-content-between") do
-          concat tag.p("#{user.name}") +
-          link_to("#{user.github_url}", user.github_url, target: "blank") +
-          link_to("View user", user_path(user.id)) +
-          link_to("Edit user", edit_user_path(user.id))
-        end
-      end.join.html_safe
+    if @users.class == String
+      tag.span(@users, class: "d-flex justify-content-center")
+    elsif @users.blank?
+      tag.span
     else
-      tag.span("#{@users}", class: "d-flex justify-content-center")
+      render_users_list
     end
   end
+end
+
+def render_users_list
+  @users.collect do |user|
+    tag.div(class: "d-flex justify-content-between") do
+      concat tag.p("#{user.name}") +
+      link_to("#{user.github_url}", user.github_url, target: "blank") +
+      link_to("View user", user_path(user.id)) +
+      link_to("Edit user", edit_user_path(user.id))
+    end
+  end.join.html_safe
 end
