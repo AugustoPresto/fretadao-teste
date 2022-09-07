23a714cbe35e3352624ccedcb3fc2438c5ab3a42
Augusto <augustodesouzacardoso@gmail.com>
2022-09-07 11:56:56 -0300

    started show page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,15 +8,25 @@ module UsersHelper
       render_users_list
     end
   end
-end
 
-def render_users_list
-  @users.collect do |user|
-    tag.div(class: "d-flex justify-content-between") do
-      concat tag.p("#{user.name}") +
-      link_to("#{user.bitlink.short_url}", user.bitlink.short_url, target: "blank") +
-      link_to("View user", user_path(user.id)) +
-      link_to("Edit user", edit_user_path(user.id))
-    end
-  end.join.html_safe
+  def render_users_list
+    @users.collect do |user|
+      tag.div(class: "d-flex justify-content-between") do
+        concat tag.p("#{user.name}") +
+        link_to("#{user.bitlink.short_url}", user.bitlink.short_url, target: "blank") +
+        link_to("View user", user_path(user.id)) +
+        link_to("Edit user", edit_user_path(user.id))
+      end
+    end.join.html_safe
+  end
+
+  def user_details
+    image_tag(@user.avatar_url, width: "200px", height: "200px", class: "rounded-circle") +
+    tag.p("Name: #{@user.name}", class: "mt-3") +
+    tag.p("GitHub nickname: #{@user.nickname}") +
+    tag.p(link_to("Visit GitHub profile", @user.bitlink.short_url, target: "blank")) +
+    tag.p("Seguidores: #{@user.followers}") +
+    tag.p("Seguindo: #{@user.following}") +
+    tag.p("Estrelas: #{@user.stars}")
+  end
 end
