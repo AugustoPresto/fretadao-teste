b7c8d7def92d957dfd9aaee985182f82c30c4099
Augusto <augustodesouzacardoso@gmail.com>
2022-09-07 11:23:31 -0300

    created simple test for bitlink model

--- spec/models/bitlink_spec.rb
+++ spec/models/bitlink_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Bitlink, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { create(:user, name: "AugustoPresto", github_url: "http://www.github.com/AugustoPresto") }
+
+  describe ".create_bitlink" do
+    it "Creates a Bitlink instance" do
+      bitlink = described_class.create_bitlink(user)
+      expect(bitlink).to be_a(Bitlink)
+      expect(bitlink.short_url).not_to be_empty
+    end
+  end
+
+  describe ".generate_short_url" do
+    it "Creates a shortened URL" do
+      short_link = described_class.generate_short_url(user.github_url)
+      expect(short_link).to include("bit.ly") 
+    end
+  end
 end
